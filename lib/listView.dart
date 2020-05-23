fc736eddb0d9ea9efa04e0ac368d8ee891d20ce4
Usman Iqbal <usmanniqbal@hotmail.com>
2020-05-23 05:27:31 +0500

    Height and width relative

--- lib/listView.dart
+++ lib/listView.dart
@@ -5,40 +5,35 @@ class AdsListingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-        elevation: 5.0,
-        splashColor: Colors.blueGrey,
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title: Text('Jobs'),
-        elevation: 17.0,
-        /* actions: <Widget>[
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+          elevation: 5.0,
+          splashColor: Colors.blueGrey,
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          title: Text('Jobs'),
+          elevation: 17.0,
+          /* actions: <Widget>[
         IconButton(onPressed: (){},
             icon: Icon(Icons.search),
           ),],*/
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            ),
+          ],
+        ),
+        body: ListView(
+          children: getMenuTiles(
+            SizedBox(
+              height: 15.0,
+              width: 15.0,
+            ),
           ),
-        ],
-      ),
-      body: ListView(
-
-
-    children: getMenuTiles(
-
-SizedBox(
-  height:15.0,
-  width:15.0,
-),
-
-      ),
-    ));
-
+        ));
   }
 
   List<Widget> getMenuTiles(SizedBox sizedBox) {
@@ -48,50 +43,50 @@ SizedBox(
           subtitle: "Need experienced developer",
           url:
               "https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png?w=70",
-      price: 2000),
-
-          MenuTileDetail(
+          price: 2000),
+      MenuTileDetail(
           title: "Fluttter developer",
           subtitle: "Need experienced flutter developer",
-          url:"https://pixabay.com/photos/flower-nature-flora-petal-summer-3140492/"
-          ),
+          url:
+              "https://pixabay.com/photos/flower-nature-flora-petal-summer-3140492/"),
       MenuTileDetail(
           title: "Node developer",
           subtitle: "Need node developer internee",
           url:
               "https://hackr.io/tutorials/flutter/logo-flutter.svg?ver=1579862938"),
-
     ];
-
 
     List<ListTile> tiles = <ListTile>[];
 
     for (var item in tileDetails) {
-
       // ignore: unnecessary_statements
-      (tiles.add(ListTile
-        (
-
+      tiles.add(ListTile(
         leading: CircleAvatar(
-
-          backgroundImage: AssetImage(item.url),
-
+            // backgroundImage: AssetImage(item.url),
+            ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(item.title),
+            Text("5000"),
+          ],
         ),
-        title: Text(item.title),
         subtitle: Text(item.subtitle),
+      ));
+    }
+    return tiles;
+  }
+}
 
-      )));}
-    return tiles;}}
-    class MenuTileDetail {
+class MenuTileDetail {
   final String title;
   final String subtitle;
   final String url;
   final int price;
   MenuTileDetail({this.title, this.subtitle, this.url, this.price});
-
 }
-class DateField{
-  final int date;
-  DateField( {this.date});
 
+class DateField {
+  final int date;
+  DateField({this.date});
 }
